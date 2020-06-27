9e95133fbdf6a1c4707fad841567d208aa175298
ouailsalem <salemouail94@gmail.com>
2020-06-27 18:35:02 +0100

    lol

--- client/src/Screens/Header.jsx
+++ client/src/Screens/Header.jsx
@@ -58,7 +58,7 @@ export const Header = () => {
         <Button
           component={Link}
           color='primary'
-          to='/products'
+          to='/affiliate'
           variant='contained'
           size='small'
           style={{
